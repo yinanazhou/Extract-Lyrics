1120e0fb45637eff976e11f33cf04fb60340221c
yinan <yinan.a.zhou@gmail.com>
2021-07-30 12:01:21 -0700

    chang %A to %a

--- dataset.sh
+++ dataset.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --time=1:00:00
 #SBATCH --array=0-4
-#SBATCH --output=output_%A.out
+#SBATCH --output=output_%a.out
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12500M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
