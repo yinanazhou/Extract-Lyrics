0b4dd9529eac662502b986503abd2f93c8a60bf7
yinan <yinan.a.zhou@gmail.com>
2021-08-01 11:04:52 -0400

    test dataset 01

--- dataset.sh
+++ dataset.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-ichiro
 #SBATCH --gres=gpu:v100:1
-#SBATCH --time=1:00:00
-#SBATCH --array=0-4
+#SBATCH --time=00:30:00
+#SBATCH --array=1-4
 #SBATCH --output=output_%a.out
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12500M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
@@ -16,5 +15,4 @@ pip install google==3.0.0
 pip install openpyxl
 
 echo "python"
-python -u divide_sheet.py --mode mw-plw
 python -u data_lyrics.py --id $SLURM_ARRAY_TASK_ID --mode mw-plw
