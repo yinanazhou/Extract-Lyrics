ba1efe98fb985831020d3032cc30a88c5e08c9bc
yinan <yinan.a.zhou@gmail.com>
2021-08-12 09:17:09 -0700

    time 02

--- data_time.sh
+++ data_time.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-ichiro
-#SBATCH --gres=gpu:v100:1
 #SBATCH --time=5-00:00
-#SBATCH --array=1,3,76,81,88,137,168,196
+#SBATCH --array=81,88
 #SBATCH --output=run_output/output_%a.out
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12500M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
