6f14589882e2c4a286b2677371315d55d9f10da2
yinan <yinan.a.zhou@gmail.com>
2021-08-21 17:30:21 -0700

    all data done 1980

--- data_time.sh
+++ data_time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-ichiro
 #SBATCH --time=5-00:00
-#SBATCH --array=81,88
+#SBATCH --array=97
 #SBATCH --output=run_output/2_output_%a.out
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12500M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
